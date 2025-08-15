7a2d1b95fd5055adef7cd08ab8c9629f18a6a6e9
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-08-15 19:43:34 +0530

    Created GlobalExceptionHandler in APIExceptiom

--- src/main/java/com/insomniacScribber/JournalApp/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/insomniacScribber/JournalApp/Exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.insomniacScribber.JournalApp.Exceptions;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,5 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(APIException.class)
-    public ResponseEntity<String> myApiException(APIException ex) {}
+    public ResponseEntity<String> myApiException(APIException ex) {
+        String message = ex.getMessage();
+        return new ResponseEntity<>(message,  HttpStatus.BAD_REQUEST);
+    }
 }
