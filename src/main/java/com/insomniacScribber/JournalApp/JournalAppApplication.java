61fb87071d750ff2170886870d9770dafe7d17cf
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-08-17 19:55:18 +0530

    @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }) //this is because Sprin automatically sets up an SQL for us but doesnt find any config for it so we are asking it to not do so

--- src/main/java/com/insomniacScribber/JournalApp/JournalAppApplication.java
+++ src/main/java/com/insomniacScribber/JournalApp/JournalAppApplication.java
@@ -2,8 +2,9 @@ package com.insomniacScribber.JournalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }) //this is because Sprin automatically sets up an SQL for us but doesnt find any config for it so we are asking it to not do so
 public class JournalAppApplication {
 
 	public static void main(String[] args) {
