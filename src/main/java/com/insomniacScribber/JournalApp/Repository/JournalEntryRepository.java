a255916ff232de5e52a1e3ea3a50c7c2cce1cc64
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-08-18 16:18:59 +0530

    Changed Id type to ObjectId

--- src/main/java/com/insomniacScribber/JournalApp/Repository/JournalEntryRepository.java
+++ src/main/java/com/insomniacScribber/JournalApp/Repository/JournalEntryRepository.java
@@ -1,11 +1,12 @@
 package com.insomniacScribber.JournalApp.Repository;
 
 import com.insomniacScribber.JournalApp.Entity.JournalEntry;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JournalEntryRepository extends MongoRepository<JournalEntry, String> {
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
 
     // Custom query methods (if needed)
     // List<JournalEntry> findByTitle(String title);
