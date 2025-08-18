f60848a022edab5d964813e09c5bf7df4bad8c53
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-08-18 16:20:27 +0530

    Changed Id type to String

--- src/main/java/com/insomniacScribber/JournalApp/Entity/JournalEntry.java
+++ src/main/java/com/insomniacScribber/JournalApp/Entity/JournalEntry.java
@@ -3,7 +3,6 @@ package com.insomniacScribber.JournalApp.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,7 +14,7 @@ import java.util.Date;
 @Document(collection = "journal_entries") // Specify the MongoDB collection
 public class JournalEntry {
     @Id
-    private ObjectId id;      // Use String for MongoDB ObjectId
+    private String id;      // Use String for MongoDB ObjectId
     private String title;
     private String content;
     private Date date;
--- src/main/java/com/insomniacScribber/JournalApp/Repository/JournalEntryRepository.java
+++ src/main/java/com/insomniacScribber/JournalApp/Repository/JournalEntryRepository.java
@@ -1,12 +1,11 @@
 package com.insomniacScribber.JournalApp.Repository;
 
 import com.insomniacScribber.JournalApp.Entity.JournalEntry;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, String> {
 
     // Custom query methods (if needed)
     // List<JournalEntry> findByTitle(String title);
