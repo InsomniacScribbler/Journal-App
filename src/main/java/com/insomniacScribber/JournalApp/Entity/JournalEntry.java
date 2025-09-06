638feaa3054ba947c2f1f7b1b4a94f7a69ddca82
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-06 20:05:14 +0530

    added date save functionality in the entity document

--- src/main/java/com/insomniacScribber/JournalApp/Entity/JournalEntry.java
+++ src/main/java/com/insomniacScribber/JournalApp/Entity/JournalEntry.java
@@ -1,36 +1,30 @@
 package com.insomniacScribber.JournalApp.Entity;
 
-import jakarta.persistence.PrePersist;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "journal_entries") // Specify the MongoDB collection
+@Document(collection = "journal_entries")
 public class JournalEntry {
     @Id
-    private String id;      // Use String for MongoDB ObjectId
+    private String id;
     @NotBlank(message = "Title can't be Blank!")
     private String title;
     @NotBlank(message = "There needs to be content in the journal Entry!")
     private String content;
-    private LocalDateTime date;
-
-    //Your JournalEntry entity doesn't automatically set the creation date.
     @CreatedDate
-    public void createDate() {
-        if (this.date == null) {
-            this.date = LocalDateTime.now();
-        }
-    }
-
+    private LocalDateTime date;
+    @LastModifiedDate
+    private LocalDateTime lastModified;
 }
