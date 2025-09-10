06926c38630fbc55632a8c10fd6df3fe3e161bb0
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-10 06:45:27 +0530

    Controller for getAllUsers

--- src/main/java/com/insomniacScribber/JournalApp/Controllers/UserController.java
+++ src/main/java/com/insomniacScribber/JournalApp/Controllers/UserController.java
@@ -1,14 +1,25 @@
 package com.insomniacScribber.JournalApp.Controllers;
 
+import com.insomniacScribber.JournalApp.Entity.User;
 import com.insomniacScribber.JournalApp.Service.UserService;
 import com.insomniacScribber.JournalApp.Service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController{
     @Autowired
     private UserService userService;
+
+    @GetMapping("/getUsers")
+    public ResponseEntity<List<User>> getUsers(){
+        List<User> users = userService.getUsers();
+        return ResponseEntity.ok().header("message", "Success").body(users);
+    }
 }
