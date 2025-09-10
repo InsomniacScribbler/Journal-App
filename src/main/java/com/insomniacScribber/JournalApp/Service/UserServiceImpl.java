834f630a9b18b814b5b2d536cb23ebc8441abd5c
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-10 06:28:24 +0530

    Impl of GetUsers

--- src/main/java/com/insomniacScribber/JournalApp/Service/UserServiceImpl.java
+++ src/main/java/com/insomniacScribber/JournalApp/Service/UserServiceImpl.java
@@ -1,7 +1,28 @@
 package com.insomniacScribber.JournalApp.Service;
 
+import com.insomniacScribber.JournalApp.Entity.User;
+import com.insomniacScribber.JournalApp.Exceptions.APIException;
+import com.insomniacScribber.JournalApp.Repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        try {
+            return userRepository.findAll();
+        } catch (Exception e) {
+            throw new APIException("Failed to retrieve journal entries: " + e.getMessage());
+        }
+    }
+
+
 }
