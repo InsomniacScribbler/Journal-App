bd6f0133f33e8e9dd4c1f65a4defa56303522098
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-10 06:52:55 +0530

    Controller for deleteUser

--- src/main/java/com/insomniacScribber/JournalApp/Controllers/UserController.java
+++ src/main/java/com/insomniacScribber/JournalApp/Controllers/UserController.java
@@ -31,4 +31,9 @@ public class UserController{
         User user = userService.getUserByUsername(username);
         return ResponseEntity.ok().header("message", "Success").body(user);
     }
+    @DeleteMapping("/deleteUser")
+    public ResponseEntity<String> deleteUser(@RequestParam String username){
+        String msg = userService.deleteUseryByUsername(username);
+        return ResponseEntity.ok().body(msg);
+    }
 }
