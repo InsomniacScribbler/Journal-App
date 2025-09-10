6e2542ee216de0bf5271bfd4d36ccdb831e5f0d2
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-10 06:41:17 +0530

    IMPL of getUsersByUsername and deleteUsersByUsername

--- src/main/java/com/insomniacScribber/JournalApp/Service/UserServiceImpl.java
+++ src/main/java/com/insomniacScribber/JournalApp/Service/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.insomniacScribber.JournalApp.Service;
 
+import com.insomniacScribber.JournalApp.Entity.JournalEntry;
 import com.insomniacScribber.JournalApp.Entity.User;
 import com.insomniacScribber.JournalApp.Exceptions.APIException;
 import com.insomniacScribber.JournalApp.Repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -40,5 +42,44 @@ public class UserServiceImpl implements UserService {
         }
     }
 
+    @Override
+    public User getUserByUsername(String username) throws APIException {
+        if (username== null || username.isBlank()) {
+            throw new APIException("Username cannot be null or empty");
+        }
+
+        try {
+            Optional<User> users = userRepository.findByUsername(username);
+            if (users.isEmpty()) {
+                throw new APIException("Entry with username " + username + " not found");
+            }
+            return users.get();
+        } catch (APIException e) {
+            throw e; // Re-throw our custom exception
+        } catch (Exception e) {
+            throw new APIException("Failed to retrieve user " + e.getMessage());
+        }
+    }
+
+    @Override
+    public String deleteUseryByUsername(String username) throws APIException {
+        if (username == null || username.isBlank()) {
+            throw new APIException("User ID cannot be null or empty");
+        }
+
+        try {
+            Optional<User> users= userRepository.findByUsername(username);
+            if (users.isEmpty()) {
+                throw new APIException("Entry with username " + username + " not found");
+            }
+            String tobeDeleted = users.get().getUsername();
+            userRepository.deleteUserByUsername(tobeDeleted);
+            return "User with username " + tobeDeleted + " deleted";
+        } catch (APIException e) {
+            throw e; // Re-throw our custom exception
+        } catch (Exception e) {
+            throw new APIException("Failed to delete journal entry: " + e.getMessage());
+        }
+    }
 
 }
