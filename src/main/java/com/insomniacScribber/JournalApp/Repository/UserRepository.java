f358a7c02a6b635a9909d3be04eaaa1c7701c1d0
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-10 06:31:52 +0530

    Repository method of finding by username

--- src/main/java/com/insomniacScribber/JournalApp/Repository/UserRepository.java
+++ src/main/java/com/insomniacScribber/JournalApp/Repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.insomniacScribber.JournalApp.Repository;
 import com.insomniacScribber.JournalApp.Entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    List<User> findByUsername(String username);
 }
