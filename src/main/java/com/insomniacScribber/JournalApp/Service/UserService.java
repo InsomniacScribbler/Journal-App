b35b9a87c4afe0037d48379d1906cfdaa57dfd79
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-10 06:27:55 +0530

    Set Up UserService

--- src/main/java/com/insomniacScribber/JournalApp/Service/UserService.java
+++ src/main/java/com/insomniacScribber/JournalApp/Service/UserService.java
@@ -1,4 +1,15 @@
 package com.insomniacScribber.JournalApp.Service;
 
+import com.insomniacScribber.JournalApp.Entity.JournalEntry;
+import com.insomniacScribber.JournalApp.Entity.User;
+
+import java.util.List;
+
 public interface UserService {
+    List<User> getUsers();
+
+    User createUser(User user);
+    User getUserById(String id);  // Changed from Long to String
+
+    String deleteUseryById(String id);
 }
