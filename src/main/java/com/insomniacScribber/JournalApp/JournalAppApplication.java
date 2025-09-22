5b76f4d0a21317e5e2610526834236978b80c8f8
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-22 19:35:47 +0530

    Added exclude DataSourceAutoConfiguration.class for main class

--- src/main/java/com/insomniacScribber/JournalApp/JournalAppApplication.java
+++ src/main/java/com/insomniacScribber/JournalApp/JournalAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }) //this is because Sprin automatically sets up an SQL for us but doesnt find any config for it so we are asking it to not do so
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }) //this is because Spring automatically sets up an SQL for us but doesnt find any config for it so we are asking it to not do so
 public class JournalAppApplication {
 
 	public static void main(String[] args) {
