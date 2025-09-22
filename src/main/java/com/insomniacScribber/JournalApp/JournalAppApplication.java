68f87b048238b01e25ab8d7f95bb6177081eee76
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-22 19:37:07 +0530

    Enabled @EnableTransactionManagement in main class

--- src/main/java/com/insomniacScribber/JournalApp/JournalAppApplication.java
+++ src/main/java/com/insomniacScribber/JournalApp/JournalAppApplication.java
@@ -3,8 +3,10 @@ package com.insomniacScribber.JournalApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }) //this is because Spring automatically sets up an SQL for us but doesnt find any config for it so we are asking it to not do so
+@EnableTransactionManagement
 public class JournalAppApplication {
 
 	public static void main(String[] args) {
