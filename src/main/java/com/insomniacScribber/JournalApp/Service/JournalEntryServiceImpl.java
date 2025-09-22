7db6b3ba998a2937488f78e4ca4d1c983441069c
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-22 19:32:17 +0530

    Updated Transaction for createJournalEntries

--- src/main/java/com/insomniacScribber/JournalApp/Service/JournalEntryServiceImpl.java
+++ src/main/java/com/insomniacScribber/JournalApp/Service/JournalEntryServiceImpl.java
@@ -7,6 +7,7 @@ import com.insomniacScribber.JournalApp.Repository.JournalEntryRepository;
 import com.insomniacScribber.JournalApp.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,6 +34,7 @@ public class JournalEntryServiceImpl implements JournalEntryService {
         }
     }
 
+    @Transactional
     @Override
     public JournalEntry createJournalEntry(JournalEntry journalEntry, String username) {
         // Validation
