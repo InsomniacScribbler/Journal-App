004062b818c7bafdbd56b27eed1568b4149c8fb3
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-22 19:45:19 +0530

    Made Bean FOR transactionManager with MongoDatabaseFactory

--- src/main/java/com/insomniacScribber/JournalApp/JournalAppApplication.java
+++ src/main/java/com/insomniacScribber/JournalApp/JournalAppApplication.java
@@ -3,6 +3,10 @@ package com.insomniacScribber.JournalApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }) //this is because Spring automatically sets up an SQL for us but doesnt find any config for it so we are asking it to not do so
@@ -11,6 +15,12 @@ public class JournalAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JournalAppApplication.class, args);
+	}
+
+
+	@Bean
+	public PlatformTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+		return new MongoTransactionManager(dbFactory);
 	}
 
 }
